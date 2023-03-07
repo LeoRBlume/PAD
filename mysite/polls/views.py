8f9e6dd94210c955fa40d3da19980db0a15182f0
ceitelabinfo <teste@teste.com>
2023-03-07 20:05:52 -0300

    Comitando template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!!")
-
-
-# Create your views here.
+    context = {
+        "msg" : "Seja bem-vindo a minha aplicacao com django.",
+        "list": [1, 2, 3, 4, 5]
+    }
+    return render(request, "index.html", context)
 
 
